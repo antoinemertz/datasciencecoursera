c4adc97f3d8258b20be3d677fdec931d36ab692d
Antoine Mertz <antoine.mertz@ge.com>
2018-04-26 17:58:13 +0200

    bug fixes

--- developping-data-products/week4/assignment/server.R
+++ developping-data-products/week4/assignment/server.R
@@ -1,6 +1,8 @@
 library(shiny)
 library(dplyr)
 library(ggplot2)
+library(randomForest)
+library(elasticnet)
 library(caret)
 library(MLmetrics)
 
@@ -8,33 +10,31 @@ data("mtcars")
 
 shinyServer(function(input, output) {
 
+  model_lm <- train(mpg ~ ., data = mtcars, method = "lm")
+
+  test_df <- mtcars %>%
+    select(-c(mpg))
+
+  model_rf <- reactive({
+    if(input$showRF) {
+      fitControl <- trainControl(method = "none")
+      rf_param <- data.frame(mtry = input$mtry_rf)
+      rf_model <- train(mpg ~ ., data = mtcars, method = "rf", trControl = fitControl, tuneGrid = rf_param)
+
+      predict(rf_model, test_df)
+    }
+  })
 
   output$svm_parameters <- renderUI({
     if(input$svr_kernel == "Linear") {
       tagList(
         sliderInput("C", "Choose cost parameter C:", min = 1e-2, max = 1-1e-2, value = 0.5, step = 1e-2)
       )
-      } else if(input$svr_kernel == "Polynomial") {
-        tagList(
-          sliderInput("C", "Choose cost parameter C:", min = 1e-2, max = 1-1e-2, value = 0.5, step = 1e-2),
-          sliderInput("degree", "Choose the degree", min = 1, max = 5, value = 2, step = 1)
-        )
-      } else if(input$svr_kernel == "RBF") {
-        tagList(
-          sliderInput("C", "Choose cost parameter C:", min = 1e-2, max = 1-1e-2, value = 0.5, step = 1e-2),
-          sliderInput("sigma", "Choose sigma parameter:", min = 1e-2, max = 1-1e-2, value = 0.5, step = 1e-2)
-        )
-      }
-  })
-
-  model_lm <- train(mpg ~ ., data = mtcars, method = "lm")
-
-
-  model_rf <- reactive({
-    if(input$showRF) {
-      fitControl <- trainControl(method = "none")
-      rf_param <- data.frame(mtry = 6)
-      train(mpg ~ ., data = mtcars, method = "rf", trControl = fitControl, tuneGrid = rf_param)
+    } else if(input$svr_kernel == "RBF") {
+      tagList(
+        sliderInput("C", "Choose cost parameter C:", min = 1e-2, max = 1-1e-2, value = 0.8, step = 1e-2),
+        sliderInput("sigma", "Choose sigma parameter:", min = 1e-2, max = 1-1e-2, value = 0.8, step = 1e-2)
+      )
     }
   })
 
@@ -43,14 +43,12 @@ shinyServer(function(input, output) {
       fitControl <- trainControl(method = "none")
       if(input$svr_kernel == "RBF") {
         svr_param <- data.frame(C = input$C, sigma = input$sigma)
-        train(mpg ~ ., data = mtcars, method = "svmRadial", tuneGrid = svr_param)
-      } else if(input$svr_kernel == "Polynomial") {
-        svr_param <- data.frame(C = input$C, degree = input$degree, scale = 100)
-        train(mpg ~ ., data = mtcars, method = "svmPoly", tuneGrid = svr_param)
+        svr_model <- train(mpg ~ ., data = mtcars, method = "svmRadial", tuneGrid = svr_param)
       } else if(input$svr_kernel == "Linear") {
         svr_param <- data.frame(C = input$C)
-        train(mpg ~ ., data = mtcars, method = "svmLinear", tuneGrid = svr_param)
+        svr_model <- train(mpg ~ ., data = mtcars, method = "svmLinear", tuneGrid = svr_param)
       }
+      predict(svr_model, test_df)
     }
   })
 
@@ -58,7 +56,8 @@ shinyServer(function(input, output) {
     if(input$showRidge) {
       fitControl <- trainControl(method = "none")
       ridge_param <- data.frame(lambda = input$lambda_ridge)
-      train(mpg ~ ., data = mtcars, method = "ridge", trControl = fitControl, tuneGrid = ridge_param)
+      ridge_model <- train(mpg ~ ., data = mtcars, method = "ridge", trControl = fitControl, tuneGrid = ridge_param)
+      predict(ridge_model, test_df)
     }
   })
 
@@ -66,135 +65,94 @@ shinyServer(function(input, output) {
     if(input$showLasso) {
       fitControl <- trainControl(method = "none")
       lasso_param <- data.frame(fraction = input$fraction_lasso)
-      train(mpg ~ ., data = mtcars, method = "lasso", trControl = fitControl, tuneGrid = lasso_param)
+      lasso_model <- train(mpg ~ ., data = mtcars, method = "lasso", trControl = fitControl, tuneGrid = lasso_param)
+      predict(lasso_model, test_df)
     }
   })
 
-   output$plot_result <- renderPlot({
+  output$plot_result <- renderPlot({
 
-     predict_lm <- mtcars %>%
-       select(-c(mpg))
+    predict_lm <- mtcars %>%
+      select(-c(mpg))
 
-     predict_lm$mpg <- predict(model_lm, predict_lm)
+    predict_lm$mpg <- predict(model_lm, predict_lm)
 
-     plot_result <- ggplot(mtcars, aes(x = mpg)) +
-       geom_point(aes(y = predict_lm$mpg, color = 'lm')) +
-       geom_abline(intercept = 0, slope = 1) +
-       xlab("True values") +
-       ylab("Predicted values") +
-       xlim(c(5, 35)) +
-       ylim(c(5, 35)) +
-       ggtitle("") +
-       theme(plot.title = element_text(hjust = 0.5))
-
+    plot_result <- ggplot(mtcars, aes(x = mpg)) +
+      geom_point(aes(y = predict_lm$mpg, color = 'lm')) +
+      geom_abline(intercept = 0, slope = 1) +
+      xlab("True values") +
+      ylab("Predicted values") +
+      xlim(c(5, 35)) +
+      ylim(c(5, 35)) +
+      ggtitle("") +
+      theme(plot.title = element_text(hjust = 0.5))
 
     if(input$showRF) {
-      predict_rf <- mtcars %>%
-        select(-c(mpg))
-
-      predict_rf$mpg <- predict(model_rf(), predict_rf)
-
       plot_result <- plot_result +
-        geom_point(aes(y = predict_rf$mpg, color = 'rf'))
+        geom_point(aes(y = model_rf(), color = 'rf'))
     }
 
     if(input$showSVR) {
-      predict_svr <- mtcars %>%
-        select(-c(mpg))
-
-      predict_svr$mpg <- predict(model_svr(), predict_svr)
-
-      if(input$svr_kernel == 'RBF') {
-        plot_result <- plot_result +
-          geom_point(aes(y = predict_svr$mpg, color = 'svr (rbf)'))
-      } else {
-        plot_result <- plot_result +
-          geom_point(aes(y = predict_svr$mpg, color = 'svr (polynomial)'))
-      }
+      plot_result <- plot_result +
+        geom_point(aes(y = model_svr(), color = 'svr'))
     }
 
     if(input$showRidge) {
-      predict_ridge <- mtcars %>%
-        select(-c(mpg))
-
-      predict_ridge$mpg <- predict(model_ridge(), predict_ridge)
-
       plot_result <- plot_result +
-        geom_point(aes(y = predict_ridge$mpg, color = 'ridge'))
+        geom_point(aes(y = model_ridge(), color = 'ridge'))
     }
 
-     if(input$showLasso) {
-       predict_lasso <- mtcars %>%
-         select(-c(mpg))
+    if(input$showLasso) {
+      plot_result <- plot_result +
+        geom_point(aes(y = model_lasso(), color = 'lasso'))
+    }
 
-       predict_lasso$mpg <- predict(model_lasso(), predict_lasso)
+    plot_result
+    })
 
-       plot_result <- plot_result +
-         geom_point(aes(y = predict_lasso$mpg, color = 'lasso'))
-     }
 
-     plot_result
-   })
 
-   output$table <- renderDataTable({
-     predict_mpg <- mtcars %>%
-       select(-c(mpg))
+  output$table <- renderDataTable({
+    predict_mpg <- mtcars %>%
+      select(-c(mpg))
 
-     prediction_lm <- predict(model_lm, predict_mpg)
+    prediction_lm <- predict(model_lm, predict_mpg)
 
-     rmspe_lm <- RMSPE(prediction_lm, mtcars$mpg)
-     mape_lm <- MAPE(prediction_lm, mtcars$mpg)
+    rmspe_lm <- RMSPE(prediction_lm, mtcars$mpg)
+    mape_lm <- MAPE(prediction_lm, mtcars$mpg)
 
-     result <- data.frame(model = c("Linear Regression (lm)"), rmspe = c(rmspe_lm), mape = mape_lm)
-     result$model <- as.character(result$model)
+    result <- data.frame(model = c("Linear Regression (lm)"), rmspe = rmspe_lm, mape = mape_lm)
+    result$model <- as.character(result$model)
 
-     if(input$showRF) {
-       prediction_rf <- predict(model_rf(), predict_mpg)
+    if(input$showRF) {
+      rmspe_rf <- RMSPE(model_rf(), mtcars$mpg)
+      mape_rf <- MAPE(model_rf(), mtcars$mpg)
 
-       rmspe_rf <- RMSPE(prediction_rf, mtcars$mpg)
-       mape_rf <- MAPE(prediction_rf, mtcars$mpg)
+      result <- rbind(result, c("Random Forest (rf)", rmspe_rf, mape_rf))
+    }
 
-       result <- rbind(result, c("Random Forest (rf)", rmspe_rf, mape_rf))
-     }
+    if(input$showSVR) {
+      rmspe_svr <- RMSPE(model_svr(), mtcars$mpg)
+      mape_svr <- MAPE(model_svr(), mtcars$mpg)
 
-     if(input$showSVR) {
-       prediction_svr <- predict(model_svr(), predict_mpg)
+      result <- rbind(result, c("SVR", rmspe_svr, mape_svr))
+    }
 
-       rmspe_svr <- RMSPE(prediction_svr, mtcars$mpg)
-       mape_svr <- MAPE(prediction_svr, mtcars$mpg)
+    if(input$showRidge) {
+      rmspe_ridge <- RMSPE(model_ridge(), mtcars$mpg)
+      mape_ridge <- MAPE(model_ridge(), mtcars$mpg)
 
-       result <- rbind(result, c("SVR", rmspe_svr, mape_svr))
-     }
+      result <- rbind(result, c("Ridge Regression (ridge)", rmspe_ridge, mape_ridge))
+    }
 
-     if(input$showRidge) {
-       prediction_ridge <- predict(model_ridge(), predict_mpg)
+    if(input$showLasso) {
+      rmspe_lasso <- RMSPE(model_lasso(), mtcars$mpg)
+      mape_lasso <- MAPE(model_lasso(), mtcars$mpg)
 
-       rmspe_ridge <- RMSPE(prediction_ridge, mtcars$mpg)
-       mape_ridge <- MAPE(prediction_ridge, mtcars$mpg)
+      result <- rbind(result, c("Lasso Regression (lasso)", rmspe_lasso, mape_lasso))
+    }
 
-       result <- rbind(result, c("Ridge Regression (ridge)", rmspe_ridge, mape_ridge))
-     }
+    result
+    })
 
-     if(input$showLasso) {
-       prediction_lasso <- predict(model_lasso(), predict_mpg)
-
-       rmspe_lasso <- RMSPE(prediction_lasso, mtcars$mpg)
-       mape_lasso <- MAPE(prediction_lasso, mtcars$mpg)
-
-       result <- rbind(result, c("Lasso Regression (lasso)", rmspe_lasso, mape_lasso))
-     }
-
-     if(input$rank_metric == 'RMSPE') {
-       result <- result %>%
-         arrange(rmspe) %>%
-         mutate(rank = row_number())
-     } else if(input$rank_metric == 'MAPE') {
-       result <- result %>%
-         arrange(mape) %>%
-         mutate(rank = row_number())
-     }
-
-     result
-   })
-
-})
+  })
--- developping-data-products/week4/assignment/ui.R
+++ developping-data-products/week4/assignment/ui.R
@@ -1,30 +1,36 @@
 library(shiny)
 library(dplyr)
 library(ggplot2)
+library(randomForest)
+library(elasticnet)
 library(caret)
 library(MLmetrics)
 
 shinyUI(fluidPage(
-  titlePanel("Predict Horsepower from MPG"),
+  titlePanel("Test different ML model"),
   sidebarLayout(
     sidebarPanel(
+      checkboxInput("showRF", h5("Show/Hide random forest model"), FALSE),
+      sliderInput(inputId = "mtry_rf", label = "Choose mtry parameter (number of variables tested at each split):", min = 2, max = 9, value = 6, step = 1),
 
-      checkboxInput("showRF", "Show/Hide random forest model", FALSE),
-
-      checkboxInput("showSVR", "Show/Hide SVR", FALSE),
-      selectInput(inputId = "svr_kernel", label = "Choose SVR kernel:", choices = c("RBF", "Polynomial", "Linear")),
+      checkboxInput("showSVR", h5("Show/Hide SVR"), FALSE),
+      selectInput(inputId = "svr_kernel", label = "Choose SVR kernel:", choices = c("RBF", "Linear")),
       uiOutput(outputId = "svm_parameters"),
 
-      checkboxInput("showRidge", "Show/Hide Ridge Regression", FALSE),
+      checkboxInput("showRidge", h5("Show/Hide Ridge Regression"), FALSE),
       sliderInput(inputId = "lambda_ridge", label = "Choose lambda parameter:", min = 1e-2, max = 1-1e-2, value = 0.5, step = 1e-2),
 
-      checkboxInput("showLasso", "Show/Hide Lasso Regression", FALSE),
+      checkboxInput("showLasso", h5("Show/Hide Lasso Regression"), FALSE),
       sliderInput(inputId = "fraction_lasso", label = "Choose fraction parameter:", min = 1e-2, max = 1-1e-2, value = 0.8, step = 1e-2),
 
-      selectInput("rank_metric", label = "Choose the ranking metric", choices = c('RMSPE', 'MAPE'))
+
+      submitButton()
 
     ),
     mainPanel(
+
+      includeMarkdown("explication.md"),
+
       plotOutput(outputId = "plot_result"),
 
       dataTableOutput('table')
