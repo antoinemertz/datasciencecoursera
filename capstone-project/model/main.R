168e9a06badab1764bc40e5691b45d0b64af000c
Antoine Mertz <antoine.mertz@ge.com>
2018-08-02 11:25:46 +0200

    model|wip

--- capstone-project/model/main.R
+++ capstone-project/model/main.R
@@ -1,14 +1,9 @@
 cat("Load libraries...", "\n")
-library(text2vec)
 library(tm)
-library(ngram)
+library(wordVectors)
 library(dplyr)
-library(tidytext)
-library(ggplot2)
-library(wordcloud)
-library(stringr)
 library(keras)
-#install_keras()
+# install_keras()
 
 cat("Load functions...", "\n")
 source('src/text_cleaning.R')
@@ -28,52 +23,45 @@ twitter <- sample(twitter, length(twitter) * size.sample)
 blogs <- sample(blogs, length(blogs) * size.sample)
 news <- sample(news, length(news) * size.sample)
 
+my_str <- c(twitter, blogs, news)
+my_str <- paste(my_str, collapse = " ")
+
 cat("Clean data...", "\n")
-twitter <- iconv(twitter,to = "ASCII",sub = "")
-blogs <- iconv(blogs,to = "ASCII",sub = "")
-news <- iconv(news,to = "ASCII",sub = "")
-
-docs <- Corpus(VectorSource(c(twitter, blogs, news)))
-
-docs <- TextCleaning(docs)
+my_str <- TextCleaning(my_str)
 
 cat("Concatenate data...", "\n")
-my_str <- stripWhitespace(paste(as.character(unlist(docs)), collapse = " "))
-
-my_str <- gsub(x = my_str, pattern = "\b", replacement = " ")
-my_str <- gsub(x = my_str, pattern = "\020", replacement = " ")
-my_str <- gsub(x = my_str, pattern = "\035", replacement = " ")
-my_str <- gsub(x = my_str, pattern = "\032", replacement = " ")
-my_str <- gsub(x = my_str, pattern = "\037", replacement = " ")
-
 my_str <- substr(my_str, 1, nchar(my_str)/100)
 
 my_words <- strsplit(my_str, " ")[[1]]
 
-# min_occurence <- 1
-# 
+min_occurence <- 1
+
 # my_words <- (data.frame(words = my_words) %>%
 #                group_by(words) %>%
 #                mutate(n = n()) %>%
 #                ungroup() %>%
 #                mutate(words = as.character(words)) %>%
 #                mutate(words = ifelse(n > min_occurence, words, "unkwn")))$words
+# 
+# my_str <- paste(my_words, collapse = " ")
+
+train_file <- "output.txt"
+
+writeLines(my_str, "output.txt")
 
 cat("Corpus length:", length(my_words), "words", "\n")
 
 # List of unique characters in the corpus
-words <- unique(my_words)
-cat("Unique words:", length(words), "\n")
+unique_words <- unique(my_words)
+cat("Unique words:", length(unique_words), "\n")
 
 maxlen <- 5 # Length of extracted words sequences
-
 step <- 3 # We sample a new sequence every `step` words
 
 cat("Create sequences...", "\n")
-next_words <- seq(maxlen + 1, length(my_words), by = step)
-sentences <- matrix(sapply(unlist(lapply(next_words, function(x) {seq(x-maxlen, x-1, by=1)})), function(x) {my_words[x]}),
-                    ncol = maxlen, byrow = TRUE)
-next_words <- sapply(next_words, function(x) {my_words[x]})
+sequences <- CreateSequences(my_words, maxlen, steps)
+sentences <- sequences$sentences
+next_words <- sequences$next_words
 
 cat("Number of sequences: ", nrow(sentences), "\n")
 
@@ -81,44 +69,18 @@ cat("Number of sequences: ", nrow(sentences), "\n")
 
 # Next, one-hot encode the characters into binary arrays.
 cat("Vectorization...", "\n")
-
-model = train_word2vec(train_file = "output.txt",
-                       output_file = "output_vectors.bin",
-                       vectors=50, force=TRUE)
-
-
-# create the vocab
-tokens <- space_tokenizer(my_str)
-it = itoken(tokens, progressbar = FALSE)
-vocab <- create_vocabulary(it)
-
-vectorizer <- vocab_vectorizer(vocab)
-
-tcm <- create_tcm(it, vectorizer, skip_grams_window = 1)
-
 word_vectors_size <- 50
+train_file <- "output.txt"
+output_file <- "output_vectors.bin"
 
-glove <- GlobalVectors$new(word_vectors_size = word_vectors_size, vocabulary = words, x_max = 10)
-
-word_vectors_main <- glove$fit_transform(tcm, n_iter = 10)
-word_vectors <- glove$components
-
-x <- array(0L, dim = c(nrow(sentences), maxlen, word_vectors_size))
-y <- array(0L, dim = c(nrow(sentences), length(words)))
-
-pb <- txtProgressBar(min = 1, max = nrow(sentences), style = 3)
-
-for (i in 1:nrow(sentences)) {
-  x[i,,] <- t(sapply(sentences[i,], function(x) {word_vectors[, which(colnames(word_vectors) == x)]}))
-  y[i,which(next_words[i] == words)] <- 1
-  Sys.sleep(0.005)
-  # update progress bar
-  setTxtProgressBar(pb, i)
-}
+train_data <- WordVectorization(train_file, output_file, word_vectors_size,
+                                min_occurence, maxlen, sentences, unique_words, next_words)
+x <- train_data$x
+y <- train_data$y
 
 model <- keras_model_sequential() %>% 
   layer_lstm(units = 128, input_shape = c(maxlen, word_vectors_size)) %>% 
-  layer_dense(units = length(words), activation = "linear")
+  layer_dense(units = length(unique_words), activation = "softmax")
 
 optimizer <- optimizer_rmsprop(lr = 0.01)
 
@@ -127,16 +89,8 @@ model %>% compile(
   optimizer = optimizer
 )
 
-sample_next_char <- function(preds, temperature = 1.0) {
-  preds <- as.numeric(preds)
-  #preds <- log(preds) / temperature
-  #exp_preds <- exp(preds)
-  #preds <- exp_preds / sum(exp_preds)
-  which.max(preds)
-}
+model %>% fit(x, y, batch_size = 128, epochs = 1)
 
-epoch <- 1
-temperature <- 1.0
 
 
 for (epoch in 1:10) {
@@ -156,14 +110,12 @@ for (epoch in 1:10) {
   
   generated_text <- seed_text
   
-  i <- 1
-  
-  sampled <- array(0, dim = c(1, maxlen, 50))
-  sampled[1,,] <- t(sapply(generated_text, function(x) {word_vectors[,x]}))
+  sampled <- array(0, dim = c(1, maxlen, word_vectors_size))
+  sampled[1,,] <- t(sapply(generated_text, function(x) {words_vectors[rownames(words_vectors) == x,]}))
   
   preds <- model %>% predict(sampled, verbose = 0)
   next_index <- sample_next_char(preds[1,], temperature)
-  next_word <- words[next_index]
+  next_word <- unique_words[next_index]
   cat("Next word predicted:", next_word, "\n")
   cat("True next word:", my_words[start_index + maxlen], "\n")
 }
