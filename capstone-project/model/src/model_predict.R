9db740f792976adb4d09dbd19f89db19bbabdb74
Antoine Mertz <antoine.mertz@ge.com>
2018-08-02 11:22:44 +0200

    model|predict next word

---
+++ capstone-project/model/src/model_predict.R
@@ -0,0 +1,38 @@
+library(dplyr)
+source("src/text_cleaning.R")
+source("src/model_train.R")
+
+text <- "The guy in front of me just bought a pound of bacon, a bouquet, and a case of"
+
+ModelPrediction <- function(text) {
+  text <- TextCleaning(text)
+  words <- strsplit(text, " ")[[1]]
+  maxlen <- length(words)
+  
+  word_vectors_size <- 100
+  min_occurence <- 1
+  
+  training <- ModelTrain(train_file = "output.txt", output_file = "output_vectors.bin", maxlen = maxlen, steps = 3,
+                         word_vectors_size = word_vectors_size, min_occurence = min_occurence)
+  cat("Model fitted...", "\n")
+  model <- training$model
+  words_vectors <- training$words_vectors
+  x <- training$x
+  y <- training$y
+  
+  prediction <- function(x) {
+    if (x %in% rownames(words_vectors)) {
+      return(words_vectors[rownames(words_vectors) == x,])
+    } else {
+      return(words_vectors[1,])
+    }
+  }
+  
+  sampled <- array(0L, dim = c(1, maxlen, word_vectors_size))
+  sampled[1,,] <- t(sapply(words, prediction))
+  
+  preds <- data.frame(proba = (model %>% predict(sampled, verbose = 0))[1,], words = rownames(words_vectors)) %>%
+    arrange(desc(proba))
+  
+  return(list(model = model, sampled = sampled, words_vectors = words_vectors, x = x, y = y, words  = words, maxlen = maxlen, word_vectors_size = word_vectors_size, preds = preds))
+}
